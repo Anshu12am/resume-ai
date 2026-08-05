f2421ae08f23e9e08c753fe4cbdedcf7db26ae86
Anshu12am <another.hell.11@gmail.com>
2026-08-05 13:36:02 +0530

    Fix Gmail SMTP configuration for Render

--- Backend/src/utils/sendMail.js
+++ Backend/src/utils/sendMail.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer');
 
-console.log("EMAIL:", process.env.EMAIL);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+
 
 const transporter = nodemailer.createTransport({
-
-  service: 'gmail',
+  service: 'gmail',  
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth:{
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS

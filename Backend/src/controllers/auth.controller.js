e1b67f393fb848586f0157198bce81de08eb2aa2
Anshu12am <another.hell.11@gmail.com>
2026-08-07 12:52:59 +0530

    Fix email config import path

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const userModel = require("../models/user.model")
 const OTP = require("../models/otp.model")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-const sendOTPEmail = require('../config/email');
+const {sendOTPEmail} = require("../../config/email")
 
 
 const generateOTP = () =>{
